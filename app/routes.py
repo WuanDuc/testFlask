6bfffe7a027ae75a9fb844200529e297ac5dd1f7
wuan.duc <univerbachtuoc@gmail.com>
2024-06-22 10:51:49 +0700

    add emotion and fix response

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,12 @@ from app import app
 from flask import make_response, render_template, send_file
 from flask import Flask, Response, request, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
+#from tensorflow.python.keras.models import load_model
+from keras.models import load_model
+#from keras.api._v2.keras.models import load_model
+from keras.layers import BatchNormalization
+import tensorflow as tf
+
 import urllib.request
 from io import BytesIO
 import base64
@@ -19,75 +25,33 @@ cloudinary.config(
   api_key = "528711498628591", 
   api_secret = "zE8uzpVsTalZQmpeRHOvUnc81Fw",
 )
+
 from cloudinary import uploader
 import cloudinary.api
 from cloudinary.utils import cloudinary_url
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+angry_count = 0
+disgust_count = 0
+fear_count = 0
+happy_count = 0
+neutral_count = 0
+sad_count = 0
+surprise_count = 0
 
-def detect():
-    #image = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
-    path = 'image.jpeg'
-    if os.path.isfile(path):
-        print("ok")
-    else:
-        print('FILE NOT FOUND')
-        return
-    image = cv2.imread(path)
-    assert not isinstance(image,type(None)), 'image not found'
-    if image is None:
-        print('Wrong path:', path)
-    else:
-        print('[RIGHT] right path:', path)
-        print('[INFO] imagesize:', image.shape)
-    image = imutils.resize(image, width=400)
-    (h, w) = image.shape[:2]
-    print(w,h)
-    print("[INFO] loading model...")
-    prototxt = 'deploy.prototxt'
-    if os.path.isfile(prototxt):
-        print("ok")
-    else:
-        print('[FILE NOT FOUND] prototxt is not found')
-        return
-    model = 'res10_300x300_ssd_iter_140000.caffemodel'
-    if os.path.isfile(model):
-        print("ok")
-    else:
-        print('[FILE NOT FOUND] model not found')
-        return
-    net = cv2.dnn.readNetFromCaffe(prototxt, model)
-    image = imutils.resize(image, width=400)
-    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
-    print("[INFO] computing object detections...")
-    net.setInput(blob)
-    detections = net.forward()
-    for i in range(0, detections.shape[2]):
 
-        # extract the confidence (i.e., probability) associated with the prediction
-        confidence = detections[0, 0, i, 2]
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence threshold
-        if confidence > 0.3:
-            # compute the (x, y)-coordinates of the bounding box for the object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
-            print(startX,startY,endX,endY)
-            # draw the bounding box of the face along with the associated probability
-            text = "{:.2f}%".format(confidence * 100)
-            y = startY - 10 if startY - 10 > 10 else startY + 10
-            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
-            cv2.putText(image, text, (startX, y),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-            cv2.imwrite("image.jpeg", image)
 def draw(image, startX, startY, endX, endY, label):
     cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
     cv2.putText(image, label, (startX, startY-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
     return image
+emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Neutral", 5:"Sad", 6:"Surprise"}
+
 
 def detect(image):
+    global angry_count, disgust_count, fear_count, happy_count, neutral_count, sad_count, surprise_count
     #image = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
     image = imutils.resize(image, width=400)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     (h, w) = image.shape[:2]
     print(w,h)
     print("[INFO] loading model...")
@@ -106,7 +70,9 @@ def detect(image):
 
     net = cv2.dnn.readNetFromCaffe(prototxt, model)
     padding = 20
-
+    emo = load_model('my_model.h5',compile=False, custom_objects={'BatchNormalization': BatchNormalization})
+    print(emo.summary())
+    #emo = load_model('my_model.h5',compile=False)
     image = imutils.resize(image, width=400)
     blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
     print("[INFO] computing object detections...")
@@ -138,20 +104,46 @@ def detect(image):
             # cv2.putText(image, text, (startX, y),
             #     cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
             t = time.time()
-            # face = image[max(0,startY-padding):min(endY+padding,image.shape[0]-1),max(0,startX-padding):min(endX+padding, image.shape[1]-1)]
-            #blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-            #net_gender.setInput(blob)
-            #genderPreds = net_gender.forward()
-            #gender = genderList[genderPreds[0].argmax()]
-            #net_age.setInput(blob)
-            #agePreds = net_age.forward()
-            #age = ageList[agePreds[0].argmax()]
+            roi_gray_frame = gray[sY:sY + h, sX:sX + w]
+            if(len(roi_gray_frame)>0):
+                cropped_img = np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), 0)
 
-            #label = "{},{}".format(gender, age)
-            #lb = label
-            #cv2.putText(image, label, (startX, startY-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+                # if len(cropped_img) > 0:
+                #     face_front += 1
+
+                # predict the emotions
+                #emotion_prediction = emo.
+                emotion_prediction = emo.predict(cropped_img, verbose=0)
+                maxindex = int(np.argmax(emotion_prediction))
+                # Calculate text size to determine placement
+                text_size = cv2.getTextSize(emotion_dict[maxindex], cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
+                text_width, text_height = text_size[0], text_size[1]
+
+                # Calculate text position
+                text_x = startX + int((endX - startX - text_width) / 2)
+                text_y = startY - 10  # Above the bounding box
+
+                # Draw the predicted emotion text on the image
+                cv2.putText(image, emotion_dict[maxindex], (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+                lb = emotion_dict[maxindex]
+                if emotion_dict[maxindex] == 'Angry':
+                    angry_count += 1
+                elif emotion_dict[maxindex] == 'Sad':
+                    sad_count += 1
+                elif emotion_dict[maxindex] == 'Neutral':
+                    neutral_count = neutral_count + 1
+                elif emotion_dict[maxindex] == 'Fear':
+                    fear_count += 1
+                elif emotion_dict[maxindex] == 'Surprise':
+                    surprise_count += 1
+                elif emotion_dict[maxindex] == 'Disgust':
+                    disgust_count += 1
+                elif emotion_dict[maxindex] == 'Happy':
+                    happy_count += 1
     return image,sX,sY,eX,eY,lb
 def detectImage():
+    global angry_count, disgust_count, fear_count, happy_count, neutral_count, sad_count, surprise_count
     path = 'image.jpeg'
     if os.path.isfile(path):
         print("ok")
@@ -168,6 +160,7 @@ def detectImage():
     output, *_ = detect(image)
     cv2.imwrite("image.jpeg", output)
 def detectVideo():
+  global angry_count, disgust_count, fear_count, happy_count, neutral_count, sad_count, surprise_count
   video_path = 'video.mp4'
   output_video_path = 'output_video.mp4'
   cap = cv2.VideoCapture(video_path)
@@ -186,9 +179,9 @@ def detectVideo():
   # Detect khuôn mặt trên frame
   frame = imutils.resize(frame, width=400)
   (h, w) = frame.shape[:2]
-
+  fourcc = cv2.VideoWriter_fourcc(*'mp4v')
   #out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mjpg'), fps, (w, h))
-  out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, (w, h))
+  out = cv2.VideoWriter(output_video_path, fourcc, fps, (w, h))
   #out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 10, (w, h))
   frame_interval = int(fps * 0.5)  # detect every half second
 
@@ -227,6 +220,7 @@ def index():
     return render_template('index.html', title='Home', user=user)
 @app.route('/image', methods=['GET', 'POST'])
 def image():
+    global angry_count, disgust_count, fear_count, happy_count, neutral_count, sad_count, surprise_count
     if(request.method == "POST"):
         if 'file' not in request.files:
             return jsonify({"error": "No file part"}), 400
@@ -249,8 +243,55 @@ def image():
             #return jsonify({"message": "File uploaded successfully", "file_path": file_path, "filename": filename}), 200
             #response = make_response(send_file(file_path,mimetype='image/png'))
             res = cloudinary.uploader.unsigned_upload(open('image.jpeg','rb'), upload_preset='videoApp', resource_type='image')
-        
-            return jsonify({'url': res['url']})
+             # Prepare response with emotion counts and Cloudinary URL
+            response = {
+                'url': res['url'],
+                'emotion_counts': [
+                        {
+                            'emoFull': 'Neutral',
+                            'emo': 'neu',
+                            'amount': neutral_count,
+                        },
+                        {
+                            'emoFull': 'Happy',
+                            'emo': 'hap',
+                            'amount': happy_count,
+                        },
+                        {
+                            'emoFull': 'Sad',
+                            'emo': 'sad',
+                            'amount': sad_count,
+                        },
+                        {
+                            'emoFull': 'Angry',
+                            'emo': 'ang',
+                            'amount': angry_count,
+                        },
+                        {
+                            'emoFull': 'Fear',
+                            'emo': 'fear',
+                            'amount': fear_count,
+                        },
+                        {
+                            'emoFull': 'Disgust',
+                            'emo': 'dis',
+                            'amount': disgust_count,
+                        },
+                        {
+                            'emoFull': 'Surprise',
+                            'emo': 'sup',
+                            'amount': surprise_count,
+                        }
+                    ]
+            }
+            angry_count = 0
+            disgust_count = 0
+            fear_count = 0
+            happy_count = 0
+            neutral_count = 0
+            sad_count = 0
+            surprise_count = 0
+            return jsonify(response)
             #return send_from_directory(app.config['UPLOAD_FOLDER'], 'image.jpeg')
         else:
             return jsonify({"error": "File upload failed"}), 400
@@ -268,6 +309,7 @@ def image():
 
 @app.route("/video", methods=['GET', 'POST'])
 def video():
+    global angry_count, disgust_count, fear_count, happy_count, neutral_count, sad_count, surprise_count
     if(request.method == "POST"):
         if 'file' not in request.files:
             return jsonify({"error": "No file part"}), 400
@@ -290,7 +332,54 @@ def video():
             #response = make_response(send_file(file_path,mimetype='video/mp4'))
             res = cloudinary.uploader.unsigned_upload(open('output_video.mp4','rb'), upload_preset='videoApp', resource_type='video')
         
-            return jsonify({'url': res['url']})
+            response = {
+                'url': res['url'],
+                'emotion_counts': [
+                        {
+                            'emoFull': 'Neutral',
+                            'emo': 'neu',
+                            'amount': neutral_count,
+                        },
+                        {
+                            'emoFull': 'Happy',
+                            'emo': 'hap',
+                            'amount': happy_count,
+                        },
+                        {
+                            'emoFull': 'Sad',
+                            'emo': 'sad',
+                            'amount': sad_count,
+                        },
+                        {
+                            'emoFull': 'Angry',
+                            'emo': 'ang',
+                            'amount': angry_count,
+                        },
+                        {
+                            'emoFull': 'Fear',
+                            'emo': 'fear',
+                            'amount': fear_count,
+                        },
+                        {
+                            'emoFull': 'Disgust',
+                            'emo': 'dis',
+                            'amount': disgust_count,
+                        },
+                        {
+                            'emoFull': 'Surprise',
+                            'emo': 'sup',
+                            'amount': surprise_count,
+                        }
+                    ]
+            }
+            angry_count = 0
+            disgust_count = 0
+            fear_count = 0
+            happy_count = 0
+            neutral_count = 0
+            sad_count = 0
+            surprise_count = 0
+            return jsonify(response)
             #return send_from_directory(app.config['UPLOAD_FOLDER'], 'image.jpeg')
         else:
             return jsonify({"error": "File upload failed"}), 400
